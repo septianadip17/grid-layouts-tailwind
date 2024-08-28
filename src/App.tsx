bcece6b5bdbcbe2b08146902198475db57fd8db3
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-28 11:29:13 +0700

    colors added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Colors from "./components/Colors";
 import CssGrid from "./components/CssGrid";
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto border-2 border-violet-500">
+    <div className="max-w-7xl mx-auto ">
       <h1 className="text-3xl font-bold text-center pt-5 border-2 border-yellow-500">Hello World</h1>
       <CssGrid />
+      <hr className="border-8 border-black m-10"/>
+      <Colors />
     </div>
   );
 }
---
+++ src/components/Colors.tsx
@@ -0,0 +1,18 @@
+const Colors = () => {
+  return (
+    <div className="grid grid-cols-3 gap-2">
+      <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-orange-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-yellow-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-green-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-blue-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-indigo-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-purple-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-pink-500 rounded-lg shadow-xl min-h-[50px]" />
+      <div className="bg-slate-500 rounded-lg shadow-xl min-h-[50px]" />
+    </div>
+  );
+};
+
+export default Colors;
