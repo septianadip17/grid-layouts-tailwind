8058c2b243758b48341567d44f2ff521ec516eab
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-28 11:22:50 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CssGrid from "./components/CssGrid";
 function App() {
   return (
     <div className="max-w-7xl mx-auto border-2 border-violet-500">
+      <h1 className="text-3xl font-bold text-center pt-5 border-2 border-yellow-500">Hello World</h1>
       <CssGrid />
     </div>
   );
--- src/components/CssGrid.tsx
+++ src/components/CssGrid.tsx
@@ -1,5 +1,43 @@
+import { data } from "../data";
+
 const CssGrid = () => {
-  return <div>CssGrid</div>;
+  const boxStyle =
+    "bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center";
+
+  return (
+    <>
+      {/* Css Grid 3x3 */}
+      <div className="grid md:grid-cols-3 auto-rows-[300px] gap-4 my-10">
+        {data.map((item, i) => (
+          <div
+            key={i}
+            className={`${boxStyle} ${
+              i === 3 || i === 6 ? "md:col-span-2" : ""
+            }`}
+          >
+            <h2 className="text-xl text-gray-600">{item.title}</h2>
+            <p className="font-bold text-2xl">{item.value} </p>
+          </div>
+        ))}
+      </div>
+        <hr className="border-8 border-violet-500"/>
+
+      {/* Css Grid 4x3 */}
+      <div className="grid md:grid-cols-4 auto-rows-[300px] gap-4 my-10">
+        {data.map((item, i) => (
+          <div
+            key={i}
+            className={`${boxStyle} ${ i === 0 ||
+              i === 4 || i === 5 || i === 6 ? "md:col-span-2" : ""
+            } ${i === 2 ? "md:row-span-2" : ""}`}
+          >
+            <h2 className="text-xl text-gray-600">{item.title}</h2>
+            <p className="font-bold text-2xl">{item.value} </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default CssGrid;
